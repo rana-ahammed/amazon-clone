7906b07127e223cdc08a8455feb6804557e253a6
ranaahammed012 <98573219+ranaahammed012@users.noreply.github.com>
2022-12-17 05:22:40 +0600

    second try

--- client/src/components/home/Slide.js
+++ client/src/components/home/Slide.js
@@ -1,10 +1,10 @@
 import React from "react";
+import "./Slide.css";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Divider from "@mui/material/Divider";
 // import { products } from "./ProductData";
-import {NavLink} from "react-router-dom";
-import "./Slide.css";
+import { NavLink } from "react-router-dom";
 
 const responsive = {
 	superLargeDesktop: {
@@ -40,16 +40,15 @@ const Slide = ({ title, products }) => {
 			<Carousel responsive={responsive} infinite={true} draggable={false} swipeable={true} showDots={false} centerMode={true} autoPlay={true} autoPlaySpeed={4000} keyBoardControl={true} removeArrowOnDevice={["mobile", "tablet"]} dotListClass="custom-dot-list-style" itemClass="carousel-item-padding-40-px" containerClass="carousel-container">
 				{products.map((e) => {
 					return (
-
 						<NavLink to={`/getProductsone/${e.id}`}>
-						<div className="products-items">
-							<div className="product-img">
-								<img src={e.url} alt="productitem" />
+							<div className="products-items">
+								<div className="product-img">
+									<img src={e.url} alt="productitem" />
+								</div>
+								<p className="product-name">{e.title.shortTitle}</p>
+								<p className="product-offer">{e.discount}</p>
+								<p className="product-explore">{e.tagline}</p>
 							</div>
-							<p className="product-name">{e.title.shortTitle}</p>
-							<p className="product-offer">{e.discount}</p>
-							<p className="product-explore">{e.tagline}</p>
-						</div>
 						</NavLink>
 					);
 				})}
